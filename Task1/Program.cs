a284bc6f3e5564004bf46cd271afb5a7e51fa40b
Simon Nevzorov <sem.nevzorov@gmail.com>
2023-03-13 09:10:37 +0300

    Домашнее задание к семинару 4

---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int Prompt(string message)
+{
+    Console.WriteLine(message); 
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+
+
+int action (int firstNumber, int secondNumber)
+{ 
+    int result = 1;
+    for (int i = 0; i < secondNumber; i++)
+        {result*= firstNumber;
+        }
+    return result;
+}
+
+
+int firstNumber = Prompt("Введите первое число");
+int secondNumber = Prompt("Введите второе число");
+
+int result = action(firstNumber, secondNumber);
+Console.WriteLine($"{firstNumber} в степени {secondNumber} = {result} ");
+
---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int Prompt(string message) // метод для введения и прочитывания числа
+{
+    Console.WriteLine(message); 
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput); // перевод строки в число
+    return result;
+}
+
+int count (int number)
+{   int result =0;
+    while (number >0)
+        { result += number % 10;
+          number = number / 10;  
+        }
+    return result;
+}
+int result = Prompt("Введите Ваше число");
+
+Console.WriteLine("Сумма чисел в числе равна = "+ count(result));
+
